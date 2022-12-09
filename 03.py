4a8984f84f672690a4ce13add9bbd72ab257564a
Meggan G <github@meggangreen.com>
2022-12-09 00:32:16 +0000

    part 2 done

--- 03.py
+++ 03.py
@@ -3,17 +3,27 @@ from inputs import T03, I03
 # toggle data for Test or Input
 data = I03
 
-rucksacks = [(chars[:len(chars)//2], chars[len(chars)//2:]) for chars in data.split('\n')]
+rucksacks = data.split('\n')
 
 vals = 0
-for rucksack in rucksacks:
-    r1, r2 = set(rucksack[0]), set(rucksack[1])
-    v = ord(list(r1 & r2)[0])
+for rs in rucksacks:
+    v = ord(list(set(rs[:len(rs)//2]) & set(rs[len(rs)//2:]))[0])
     if v >= 97:
         vals += v - 96
     else:
         vals += v - 38
 
-
 print(f"Part 1: {vals}")    # 7766
 
+
+vals = 0
+i = 0
+while i < len(rucksacks):
+    v = ord(list(set(rucksacks[i+0]) & set(rucksacks[i+1]) & set(rucksacks[i+2]))[0])
+    if v >= 97:
+        vals += v - 96
+    else:
+        vals += v - 38
+    i += 3
+
+print(f"Part 2: {vals}")    # 2415
