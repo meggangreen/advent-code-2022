e595c6f62af53f74b8fb13c0d2e2512f0c77bb5e
Meggan G <github@meggangreen.com>
2022-12-09 01:21:07 +0000

    day 4 done

--- 04.py
+++ 04.py
@@ -1,7 +1,7 @@
-from inputs import T04
+from inputs import T04, I04
 
 # toggle data for Test or Input
-data = T04
+data = I04
 
 pairs = [[[int(n) for n in assign.split('-')] for assign in line.split(',')] for line in data.split('\n')]
 
@@ -15,4 +15,17 @@ for pair in pairs:
     if (((L1 <= L2 ) & (U1>=U2)) | ((L2 <= L1 ) & (U2>=U1))):
         count += 1
 
-print(count)
+print(f"Part 1: {count}")    # 413
+
+
+count = 0
+for pair in pairs:
+    L1 = pair[0][0]
+    U1 = pair[0][1]
+    L2 = pair[1][0]
+    U2 = pair[1][1]
+
+    if (((L1 <= L2) & (U1>=U2)) | ((L2 <= L1) & (U2>=U1)) | ((U1 >= L2)  & (L1<=U2))):
+        count += 1
+
+print(f"Part 2: {count}")
