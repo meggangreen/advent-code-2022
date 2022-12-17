989dd332eeac7b4d83ba92e579b9f00dfa411851
Meggan G <github@meggangreen.com>
2022-12-17 00:00:01 +0000

    p1 done

--- 06.py
+++ 06.py
@@ -1,5 +1,10 @@
 from inputs import T06, I06
 
 # toggle data for Test or Input
-data = T06.split('\n')
+data = I06.split('\n')
 
+for d in data:
+    for i in range(3,len(d)):
+        if len(set(d[i-3:i+1])) == 4:
+            print(f"Part 1: {i+1}")    # 1723
+            break
