3e7c60cab898cf673b38e3f2d76e405e3cf859c5
Meggan G <github@meggangreen.com>
2022-12-17 00:03:30 +0000

    day 6 done

--- 06.py
+++ 06.py
@@ -3,8 +3,16 @@ from inputs import T06, I06
 # toggle data for Test or Input
 data = I06.split('\n')
 
+# find packet marker
 for d in data:
     for i in range(3,len(d)):
         if len(set(d[i-3:i+1])) == 4:
             print(f"Part 1: {i+1}")    # 1723
+            break
+
+# find message marker
+for d in data:
+    for i in range(13,len(d)):
+        if len(set(d[i-13:i+1])) == 14:
+            print(f"Part 2: {i+1}")    # 3708
             break
