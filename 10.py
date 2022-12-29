91a74bb45ea9b245a324a2ae1c6cc1a6f81257b7
Meggan G <github@meggangreen.com>
2022-12-29 18:13:25 +0000

    10 done

--- 10.py
+++ 10.py
@@ -3,8 +3,8 @@ from inputs import T10, I10
 # toggle data for Test or Input
 data = I10.split('\n')[::-1]
 
-cycles, register = 0, 1
-strength, crt = [], []
+cycles, register, strength = 0, 1, []
+crt = [['.'] * 40, ['.'] * 40, ['.'] * 40, ['.'] * 40, ['.'] * 40, ['.'] * 40]
 instructions = {'addx': 2, 'noop': 1}
 while data:
     inst = data.pop().split()
@@ -12,7 +12,15 @@ while data:
         cycles += 1
         if cycles in [20, 60, 100, 140, 180, 220]:
             strength.append(cycles * register)
+        row = cycles // 41
+        pixel = (cycles - 1) % 40
+        if (register -1) <= pixel <= (register+1):
+            crt[row][pixel] = '#'
     if inst[0] == 'addx':
         register += int(inst[1])
 
+for i, row in enumerate(crt):
+    crt[i] = ''.join(row)
+
 print(f"Part 1: {sum(strength)}")   # 13920
+print("Part 2:", '\n'.join(crt), sep='\n')  # EGLHBLFJ
